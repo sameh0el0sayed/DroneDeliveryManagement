5aacd8dafb5153c5c800c71721f8267cd432388d
Sameh AlSayed Rajab <samehsr.work@gmail.com>
2026-01-20 18:31:55 +0300

    first commit

---
+++ src/test/java/com/example/DroneApp/DroneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DroneApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
